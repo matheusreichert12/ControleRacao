c144d04127bbc6a053c930a674c3e04068959b7f
Matheus Vinicius Reichert <matheus.reichert@abase.local>
2019-10-15 15:50:53 -0300

    iniciando projeto

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:cachorros/ui/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+    ));
